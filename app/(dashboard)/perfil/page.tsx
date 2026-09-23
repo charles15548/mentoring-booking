749624256ae1e23eacb734d321596c5c72e6a109
charles15548 <charleshuamanllaccta@gmail.com>
2026-09-23 15:25:10 -0500

    perfil, error

---
+++ app/(dashboard)/perfil/page.tsx
@@ -0,0 +1,8 @@
+export default function PerfilPage() {
+  return (
+    <section aria-labelledby="perfil-title">
+      <h1 id="perfil-title">Mi perfil</h1>
+      <p>Administra la información de tu perfil.</p>
+    </section>
+  );
+}
